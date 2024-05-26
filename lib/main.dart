46c56ec920f252747b963a17126af75b1589ff60
GerardoFJ <A00838645@tec.mx>
2024-05-26 04:40:03 -0600

    Finished login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,28 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
-
+import 'pages/login.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-        options: DefaultFirebaseOptions.currentPlatform,
-      );
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
-  
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Start'),
     );
   }
 }
@@ -37,49 +36,112 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   FirebaseFirestore db = FirebaseFirestore.instance;
-  int _counter = 0;
+  bool isUseractive = false;
 
-  void _incrementCounter() {
-    setState((){
-      _counter++;
-      final counter = <String, dynamic>{
-        "first": _counter
-      };
-      db.collection("counters").add(counter).then((DocumentReference doc) =>
-        print('DocumentSnapshot added with ID: ${doc.id}'));
-    });
+ @override
+  void initState(){
+    super.initState();
+   FirebaseAuth.instance
+  .authStateChanges()
+  .listen((User? user) {
+    if (user == null) {
+      setState(() {
+        isUseractive = false;
+      });
+      print("User not active");
+    } else {
+      setState(() {
+        isUseractive = true;
+      });
+      print("User active!");
+    }
+  });
   }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color.fromARGB(0, 0, 0, 0),
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 0, 0, 0),
+        
+        leading:
+        Visibility(
+          visible: isUseractive,
+        child: Row(mainAxisSize: MainAxisSize.min, children: <Widget>[
+               new GestureDetector(
+                  onTap: ()async{
+  
+                  await FirebaseAuth.instance.signOut();
+                  },
+                child: Container(
+                  
+                  decoration: BoxDecoration(
+                      color: Color.fromARGB(255, 255, 255, 255),
+                      borderRadius: const BorderRadius.all(Radius.circular(10))),
+                padding: const EdgeInsets.symmetric(
+                 horizontal: 10.0),
+                child: Center(
+                  child: Text(
+                    "Logout",
+                    style: TextStyle(
+                        color: Color.fromARGB(255, 0, 0, 0),
+                        fontSize: 10.0),
+                  ),
+                ),
+              ),),
+            ]),),
+
+
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
- 
-   Container(
-    decoration: BoxDecoration(
-    color: const Color(0xff7c94b6),
-    border: Border.all(
-    ),
-    borderRadius: const BorderRadius.all(Radius.circular(10))
-  ),
-  width: 100.0,
-    height: 48.0,
-
-  ),
-  Container(
-    margin: const EdgeInsets.all(10.0),
-    color: Colors.amber[600],
-    width: 48.0,
-    height: 48.0,
+              Row(mainAxisSize: MainAxisSize.min, children: <Widget>[
+               new GestureDetector(
+                  onTap: (){
   
-),
+                   Navigator.push(context, MaterialPageRoute(builder: (context) => LoginPage()));
+                  },
+                child: Container(
+                  
+                  decoration: BoxDecoration(
+                      color: Color.fromARGB(255, 255, 255, 255),
+                      borderRadius: const BorderRadius.all(Radius.circular(10))),
+                padding: const EdgeInsets.symmetric(
+                    vertical: 10.0, horizontal: 16.0),
+                child: Center(
+                  child: Text(
+                    "Login page",
+                    style: TextStyle(
+                        color: Color.fromARGB(255, 0, 0, 0),
+                        fontSize: 30.0),
+                  ),
+                ),
+              ),),
+            ]),
+             Row(mainAxisSize: MainAxisSize.min, children: <Widget>[
+                Container(
+                  margin: const EdgeInsets.symmetric(vertical: 20.0),
+                  decoration: BoxDecoration(
+                      color: Color.fromARGB(255, 255, 255, 255),
+                      borderRadius: const BorderRadius.all(Radius.circular(10))),
+                padding: const EdgeInsets.symmetric(
+                    vertical: 10.0, horizontal: 16.0),
+                child: Center(
+                  child: Text(
+                    "Register page",
+                    style: TextStyle(
+                        color: Color.fromARGB(255, 0, 0, 0),
+                        fontSize: 30.0),
+                  ),
+                ),
+              ),
+            ]),
 
           ],
         ),
-      ), 
+      ),
     );
   }
 }
---
+++ lib/pages/login.dart
@@ -0,0 +1,175 @@
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:foraneoapp/main.dart';
+import '../firebase_options.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+Future<bool> login_user(String email, String password)async{
+try {
+        final credential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+          email: email,
+          password: password
+        );
+        return true;
+      } on FirebaseAuthException catch (e) {
+       return false;
+      }
+}
+
+String? email_validator(value) {
+  final RegExp emailRegex = RegExp(r'^[^@]+@[^@]+\.[^@]+$');
+   if (value == null || value.isEmpty || !emailRegex.hasMatch(value)) {
+                return 'Ingresa un email valido';
+              }
+              return null;
+}
+
+String? password_validator(value) {
+   if (value == null || value.isEmpty) {
+                return 'Ingresa una contraseña valida';
+              }
+              return null;
+}
+
+
+Widget Input_forms(Icon left_icon, String hidden_text, String validator_func, TextEditingController controller_data){
+  return Container(
+              margin: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 5.0),
+              child:TextFormField(
+            controller: controller_data,
+            style: TextStyle(color:Color.fromARGB(255, 255, 255, 255)),
+            obscureText: validator_func == "Password" ? true : false,
+            decoration: InputDecoration(
+              hintText: hidden_text,
+    
+              hintStyle: TextStyle(color:Color.fromARGB(133, 255, 255, 255)),
+                icon: left_icon
+            ),
+            validator: (String? value) {
+              if(validator_func == "Email"){
+              return email_validator(value);
+              }
+              else{
+                return password_validator(value);
+              }
+            },
+
+          ),);
+}
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+
+class _LoginPageState extends State<LoginPage> {
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  bool wrong_data = false;
+  @override
+  void initState(){
+    super.initState();
+   FirebaseAuth.instance
+  .authStateChanges()
+  .listen((User? user) {
+    if (user == null) {
+      print('User is currently signed out!');
+    } else {
+      print('User is signed in!');
+    }
+  });
+  
+  
+   // Call your function here
+  }
+ 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 0, 0, 0),
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(1, 0, 0, 0),
+        leading:new GestureDetector(child:Icon(Icons.arrow_back,size: 25.0, color: Color.fromARGB(255, 255, 255, 255), 
+        
+        ),
+        onTap: ()  {
+          Navigator.push(context, MaterialPageRoute(builder: (context) => MyHomePage(title: 'Main page',)));
+        },),
+      ),    
+      body: Center(
+        child: Form(
+          key: _formKey,
+          child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+          Text("Iniciar Sesion",
+                    style: TextStyle(
+                        color: Color.fromARGB(255, 255, 255, 255),
+                        fontSize: 50.0,
+                        fontFamily: "Helvetica",
+                        fontWeight: FontWeight.bold),
+                  ),
+
+          Input_forms(Icon( Icons.email,
+      color: Color.fromARGB(255, 207, 207, 207),
+      size: 24.0,
+    ),"Ingresa tu email","Email",emailController),
+            Input_forms(Icon( Icons.password,
+      color: Color.fromARGB(255, 207, 207, 207),
+      size: 24.0,
+    ),"Contraseña","Password",passwordController),
+    Visibility(visible: wrong_data,child: Text("El correo o contraseña son incorrectos, revise sus datos.", 
+    style: TextStyle(color: Color.fromARGB(185, 255, 0, 0), fontSize: 15.0, fontFamily: "Helvetica",), 
+    textAlign: TextAlign.center, 
+    )),
+              Row(mainAxisSize: MainAxisSize.min, children: <Widget>[
+               new GestureDetector(
+                  onTap: ()async {
+                    if (_formKey.currentState!.validate()) {
+                      setState(() {
+                           wrong_data = false;
+                         });
+                        print(emailController.text.trim());
+                        print(passwordController.text.trim());
+                        if (await login_user(emailController.text.trim(),passwordController.text.trim())){
+                          //navigate
+                        }
+                        else{
+                         setState(() {
+                           wrong_data = true;
+                         });
+                          
+                        }
+                    }  
+                
+                  },
+                child: Container(
+                  margin:EdgeInsets.symmetric(vertical: 10.0),
+                  decoration: BoxDecoration(
+                      color: Color.fromARGB(255, 255, 255, 255),
+                      borderRadius: const BorderRadius.all(Radius.circular(50))),
+                padding: const EdgeInsets.symmetric(
+                    vertical: 10.0, horizontal: 40.0),
+                child: Center(
+                  child: Text(
+                    "Login",
+                    style: TextStyle(
+                        color: Color.fromARGB(255, 0, 0, 0),
+                        fontSize: 30.0),
+                  ),
+                ),
+              ),),
+            ]),
+             
+
+          ],
+        ),),
+      ),
+    );
+  }
+}
